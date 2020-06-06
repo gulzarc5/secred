61e4935d25ee090e87ede5e0372a8b6267350a6e
imvishalnag <imvishalnag@gmail.com>
2020-06-07 03:07:08 +0530

    update

---
+++ resources/views/web/academics.blade.php
@@ -0,0 +1,128 @@
+@extends('web.templete.master')
+
+{{-- META --}}
+@section('meta')
+	<title>Parakiaki Academy</title>
+	<!--<meta name="description" content="">-->
+	<!--<meta name="author" content="">-->
+	<!--<meta name="keywords" content="">-->
+@endsection
+
+@section('content') 
+<style>
+    .first::first-letter 
+		{
+		    font-size: 200%;
+		    color: black;
+		    line-height:1;
+		}
+</style>
+
+    <!-- NEWS AND EVENTS -->
+    <section class="h-quote">
+        <div class="container pad-bot-0">
+            <div class="row">
+                <div class="con-title">
+                    <h2>Academics </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy" style="margin-bottom: 20px">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px">Subjects Offered</h4>
+                        <p class="first">Our College provides education to the students of Arts & Commerce Stream. In both the streams, students have to select at-least th ree or four subjects of their choice in addition to the core subjects mentioned here under.</p>
+                    </div>
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy" style="margin-bottom: 20px">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px">Core Subjects (All Stream)</h4>
+                        <p>1. 	English</p>
+                        <p>2.  	MIL (Bengali/Alt. English/ Manipuri)</p>
+                    </div>
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy" style="margin-bottom: 20px">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px">Other Subjects (Arts)</h4>                                
+                        <p>1.  Economics
+                        <p>2.  Political Science
+                        <p>3.  Education
+                        <p>4.  Logic & Philosophy
+                        <p>5.  Advance Bengali
+                        <p>6.  Persian
+                    </div>
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy" style="margin-bottom: 20px">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px"> Other Subjects (Commerce)</h4>                               
+                        <p>1.  Economics</p>
+                        <p>2.  Accountancy</p>
+                        <p>3.  Business Studies</p>
+                        <p>4.  Commercial Math & Statistics</p>
+                        <p>5.  Banking</p>
+                    </div>
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px">Required Documents</h4>
+                        <p>Students have to enclose the self attested Xerox copies of the following documents while submitting the application form for admission. They have to show the original documents at the time of admission for verification.</p>
+                        <p>1. 	Admit Card Of The Last Examination.</p>
+                        <p>2.   Provisional School Certificate Form The School Last Attended.</p>
+                        <p>3.  	Study Gap Certificate (If Any).</p>
+                        <p>4.  	Migration Certificate (If Any).</p>
+                        <p>5. 	1 Passport Size Photo.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy" style="margin-bottom: 20px">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px">Fee Structure</h4>
+                        <p class="first">Student must pay their admission fees at the time taking admission to the college apart from that students can pay their monthly tuition fees at a time/quaterly/monthly. Relaxation in the admission fees will be offered to the 80% + marks holders in HSLC Exam. Special scholarships are available for poor meritorious students.</p>
+                    </div>
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy" style="margin-bottom: 20px">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px">Fee Structure</h4>
+                        <div class="pro-con-table">
+                            <table class="bordered responsive-table">
+                                <thead>
+                                    <tr style="background: #f36b3b!important;">
+                                        <th class="text-white">Arts </th>
+                                        <th class="text-white">Commerce</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr style="background: transparent!important;">
+                                        <td>Rs. 5800.00*</td>
+                                        <td>Rs. 6500.00*</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy" style="margin-bottom: 20px">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px">Tuition Fees (Monthly)</h4>
+                        <div class="pro-con-table">
+                            <table class="bordered responsive-table">
+                                <thead>
+                                    <tr style="background: #f36b3b!important;">
+                                        <th class="text-white">Arts </th>
+                                        <th class="text-white">Commerce</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr style="background: transparent!important;">
+                                        <td>Rs. 800.00</td>
+                                        <td>Rs. 900.00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="bot-gal h-vid ho-event-mob-bot-sp p-justfy" style="margin-bottom: 20px">
+                        <h4 style="border-color: #f26838; margin-bottom: 10px">Admission procedure</h4>
+                        <p>1. 	Two years Higher Secondary Course in ARTS & Commerce are governed by the rules & regulations of Assam Higher Secondary Education Council.</p>
+                        <p>2.  	The prospectus & admission form can be collected from the college office during office hours and students may download prospectus and admission form from www.sacredheartsss.com</p>
+                        <p>3.  	The admission is totally based on the merit and past result of the students.</p>
+                        <p>4.  	Presence of Parent/Guardian is mandatory during the time of admission.</p>
+                        <p>5. 	Students having good percentage of marks in HSLC Exam can get preference for admission.</p>
+                        <p>6. 	List of selected candidates will be displayed on notice board.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection   
+
+      
--- resources/views/web/include/footer.blade.php
+++ resources/views/web/include/footer.blade.php
@@ -313,4 +313,42 @@
             $("#facebook-div").html(data);  
 
         }
+    </script> 
+    <script>
+        jQuery.fn.liScroll = function(settings) {
+            settings = jQuery.extend({
+                travelocity: 0.08
+                }, settings);		
+                return this.each(function(){
+                        var $strip = jQuery(this);
+                        $strip.addClass("newsticker")
+                        var stripHeight = 0;
+                        $strip.find("li").each(function(i){
+                            stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+                        });
+                        var $mask = $strip.wrap("<div class='mask'></div>");
+                        var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
+                        var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
+                        $strip.height(stripHeight);			
+                        var totalTravel = stripHeight;
+                        var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
+                        function scrollnews(spazio, tempo){
+                        $strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", "40%"); scrollnews(totalTravel, defTiming);});
+                        }
+                        scrollnews(totalTravel, defTiming);				
+                        $strip.hover(function(){
+                        jQuery(this).stop();
+                        },
+                        function(){
+                        var offset = jQuery(this).offset();
+                        var residualSpace = stripHeight;
+                        var residualTime = residualSpace/settings.travelocity;
+                        scrollnews(residualSpace, residualTime);
+                        });			
+                });	
+        };
+
+        $(function(){
+            $("ul#ticker01").liScroll();
+        });
     </script>
